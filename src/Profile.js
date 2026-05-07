6a02c0336aef503845ecc831ae4257d86d476130
Ayisha <ayisha07in@gmail.com>
2026-05-07 09:36:27 +0530

    Fix unused import in Profile.js

--- src/Profile.js
+++ src/Profile.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 
 function Profile({ accessibility, setAccessibility }) {
   const [name, setName] = useState(localStorage.getItem("empathaiName") || "");
